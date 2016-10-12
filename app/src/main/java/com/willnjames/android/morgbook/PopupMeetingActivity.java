3cc64819ad109537f1c18ea08b752782cc53a7d4
James Prijatna <z5018453@zmail.unsw.edu.au>
2016-10-13 00:43:03 +1100

    Added a Popup -> Look at the last tab and click on the Button to see it -> Rather than a dialog, it uses an activity which will be useful when we want to do computations and what not

---
+++ app/src/main/java/com/willnjames/android/morgbook/PopupMeetingActivity.java
@@ -0,0 +1,19 @@
+package com.willnjames.android.morgbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jamesprijatna on 13/10/16.
+ */
+
+public class PopupMeetingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity3);
+    }
+}
--- app/src/main/java/com/willnjames/android/morgbook/SecondActivity.java
+++ app/src/main/java/com/willnjames/android/morgbook/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.willnjames.android.morgbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextClock;
 
 
@@ -11,6 +14,7 @@ import android.widget.TextClock;
 public class SecondActivity extends Activity {
 
     private TextClock textClock;
+    private Button popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,18 @@ public class SecondActivity extends Activity {
         setContentView(R.layout.activity2);
 
         textClock = (TextClock) findViewById(R.id.textClock2);
+        popup = (Button) findViewById(R.id.popup);
+
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(SecondActivity.this, PopupMeetingActivity.class);
+                SecondActivity.this.startActivity(myIntent);
+
+
+            }
+        });
 
     }
 }
